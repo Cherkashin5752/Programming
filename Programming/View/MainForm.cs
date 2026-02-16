eeaec2a8b1a54d7e13c60998bad91e4e4776b91e
Савелий Черкашин <greftrack@gmail.com>
2026-02-16 15:27:55 +0700

    return to default Text File

--- Programming/View/MainForm.cs
+++ Programming/View/MainForm.cs
@@ -253,7 +253,7 @@ namespace Programming
             if (!int.TryParse(WeekdayTextBox.Text, out var a) &&
                 Enum.TryParse<Weekday>(WeekdayTextBox.Text, true, out Weekday weekday))
             {
-                IsitWeekday.Text = $"Есть такой день недели({weekday} = {(int)weekday + 1})";
+                IsitWeekday.Text = $"Есть такой день недели ({weekday} = {(int)weekday + 1})";
                 return;
             }
 
